ce5d90a37bcd036dfadf6794c97d640bf3093063
Imissrain <490646528@qq.com>
2018-06-02 14:16:32 +0800

    用一个Map来存储 放入url 同时i++ 取出的时候将短url替换成空串取出即可

---
+++ src/String/Codec.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 535. TinyURL 的加密与解密
+ * 题目描述提示帮助提交记录社区讨论阅读解答
+ * TinyURL是一种URL简化服务， 比如：当你输入一个URL https://leetcode.com/problems/design-tinyurl 时，它将返回一个简化的URL http://tinyurl.com/4e9iAk.
+ *
+ * 要求：设计一个 TinyURL 的加密 encode 和解密 decode 的方法。你的加密和解密算法如何设计和运作是没有限制的，你只需要保证一个URL可以被加密成一个TinyURL，并且这个TinyURL可以用解密方法恢复成原本的URL。
+ * */
+public class Codec {
+    //用一个Map来存储 放入url 同时i++ 取出的时候将短url替换成空串取出即可
+    Map<Integer, String> map = new HashMap<>();
+    int i=0;
+    public String encode(String longUrl) {
+        map.put(i,longUrl);
+        return "http://tinyurl.com/"+i++;
+    }
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+}
