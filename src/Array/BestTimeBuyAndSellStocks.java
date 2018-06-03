596b522484f771e20d8d44baf849bfa702a9fdc9
Imissrain <490646528@qq.com>
2018-06-03 23:22:36 +0800

    sum相邻差值大于0的

--- src/Array/BestTimeBuyAndSellStocks.java
+++ src/Array/BestTimeBuyAndSellStocks.java
@@ -31,7 +31,6 @@ public class BestTimeBuyAndSellStocks {
         if(prices.length==0)
             return 0;
         int res=0;
-
         //sum相邻差值大于0的
         for(int i=0;i<prices.length-1;i++){
             if((prices[i+1]-prices[i])>0)
