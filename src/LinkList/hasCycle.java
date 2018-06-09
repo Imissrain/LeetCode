fe847914d8bd7cb4721cb236ecf9f75a72b25e21
Imissrain <490646528@qq.com>
2018-06-09 22:18:43 +0800

    总体思路：利用快慢指针解决 慢的每次走1 快的每次走2 当他俩相等的时候说明链表是有环的

--- src/LinkList/hasCycle.java
+++ src/LinkList/hasCycle.java
@@ -14,7 +14,6 @@ public class hasCycle {
         ListNode(int x) { val = x; }
     }
     //总体思路：利用快慢指针解决 慢的每次走1 快的每次走2 当他俩相等的时候说明链表是有环的
-
     public boolean hasCycle(ListNode head) {
 //        if(head==null)
 //            return false;
