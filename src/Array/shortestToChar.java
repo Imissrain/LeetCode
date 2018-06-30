20d47ea5699ae616be785e4912da5ceaf2036673
Imissrain <490646528@qq.com>
2018-06-30 23:59:43 +0800

    转成二进制字符串然后前后相比 || 位运算

---
+++ src/Array/shortestToChar.java
@@ -0,0 +1,27 @@
+package Array;
+
+import org.junit.Test;
+
+/**
+ * 821. 字符的最短距离
+ * 题目描述提示帮助提交记录社区讨论阅读解答
+ * 给定一个字符串 S 和一个字符 C。返回一个代表字符串 S 中每个字符到字符串 S 中的字符 C 的最短距离的数组。
+ *
+ * 示例 1:
+ *
+ * 输入: S = "loveleetcode", C = 'e'
+ * 输出: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+ * 说明:
+ *
+ *
+ * 字符串 S 的长度范围为 [1, 10000]。
+ * C 是一个单字符，且保证是字符串 S 里的字符。
+ * S 和 C 中的所有字母均为小写字母。
+ * */
+@SuppressWarnings("all")
+public class shortestToChar {
+    public int[] shortestToChar(String S, char C) {
+        int arr[]=new int[S.length()];
+        return null;
+    }
+}
