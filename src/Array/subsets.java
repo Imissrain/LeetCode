5baa233faa047f8ec882c078cb1b8a758bb30c7a
Imissrain <490646528@qq.com>
2018-07-04 19:35:31 +0800

    正反两次遍历 每次记录C的位置 第一次遍历如果等于C flag记录C的最新下标  arr[i]就等于当前下标减去flag

---
+++ src/Array/subsets.java
@@ -0,0 +1,32 @@
+package Array;
+
+import java.util.List;
+
+/**
+ * 78. 子集
+ * 题目描述提示帮助提交记录社区讨论阅读解答
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ *
+ * 说明：解集不能包含重复的子集。
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,2,3]
+ * 输出:
+ * [
+ *   [3],
+ *   [1],
+ *   [2],
+ *   [1,2,3],
+ *   [1,3],
+ *   [2,3],
+ *   [1,2],
+ *   []
+ * ]
+ * */
+@SuppressWarnings("all")
+public class subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        return null;
+    }
+}
