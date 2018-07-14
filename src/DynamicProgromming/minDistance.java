8eaf204d802e82acab7027900895821617c10032
Imissrain <490646528@qq.com>
2018-07-14 18:41:32 +0800

    dp[i][j]代表第一个字符串从0带i  到第二个字符串从0到j  形成的子串的编辑距离 相等的话无需操作 不等的话 插入:dp[i][j-1]+1 替换:dp[i-1][j-1]+1 删除:dp[i-1][j]+1 取最小值

--- src/DynamicProgromming/minDistance.java
+++ src/DynamicProgromming/minDistance.java
@@ -30,7 +30,35 @@ package DynamicProgromming;
  * */
 @SuppressWarnings("all")
 public class minDistance {
-    public int minDistance(String word1, String word2) {
-        return 0;
+    public static int minDistance(String word1, String word2) {
+        //dp[i][j]代表第一个字符串从0带i  到第二个字符串从0到j  形成的子串的编辑距离 相等的话无需操作 不等的话 插入:dp[i][j-1]+1 替换:dp[i-1][j-1]+1 删除:dp[i-1][j]+1 取最小值
+        if(word1.length()==0)
+            return word2.length();
+        if(word2.length()==0)
+            return word1.length();
+        int dp[][]=new int[word1.length()+1][word2.length()+1];
+        dp[0][0]=0;
+        for(int i=1;i<=word1.length();i++){
+            dp[i][0]=i;
+        }
+        for(int j=0;j<=word2.length();j++){
+            dp[0][j]=j;
+        }
+        for(int i=1;i<=word1.length();i++){
+            for(int j=1;j<=word2.length();j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }else {
+                    dp[i][j]=Math.min(Math.min(dp[i][j-1]+1,dp[i-1][j]+1),dp[i-1][j-1]+1);
+                }
+            }
+        }
+        return dp[word1.length()][word2.length()];
+    }
+
+    public static void main(String[] args) {
+        String word1="abc";
+        String word2="bbc";
+        System.out.println(minDistance(word1,word2));
     }
 }
