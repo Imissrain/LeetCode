3aada1cc91bfec60333f83ba1880f12efd6c0239
Imissrain <490646528@qq.com>
2018-08-08 19:54:51 +0800

    淦

---
+++ src/LinkList/addTwoNumbers.java
@@ -0,0 +1,37 @@
+package LinkList;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.*;
+
+/**
+ * 445. 两数相加 II
+ * 题目描述提示帮助提交记录社区讨论阅读解答
+ * 给定两个非空链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储单个数字。将这两数相加会返回一个新的链表。
+ *
+ *
+ *
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ *
+ * 进阶:
+ *
+ * 如果输入链表不能修改该如何处理？换句话说，你不能对列表中的节点进行翻转。
+ */
+@SuppressWarnings("all")
+public class addTwoNumbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    @Test
+    public void fun(){
+
+    }
+}
