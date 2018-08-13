ff7be297a4cf5f0f8137c4db620274bb743d7e92
Imissrain <490646528@qq.com>
2018-08-13 20:16:22 +0800

    坑

---
+++ src/LinkList/mergeKLists.java
@@ -0,0 +1,34 @@
+package LinkList;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 23. 合并K个排序链表
+ * 题目描述提示帮助提交记录社区讨论阅读解答
+ * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * 输出: 1->1->2->3->4->4->5->6
+ * */
+@SuppressWarnings("all")
+public class mergeKLists {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+
+        return null;
+    }
+}
