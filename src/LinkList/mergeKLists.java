1082219ad5480b1e041e2f226c1a88c97b643485
Imissrain <490646528@qq.com>
2018-08-14 13:49:53 +0800

    暴力 先搞出来两个链表相加 然后从i=1开始 每一位的值都等于加上前一位的值 最后返回末尾呢个

--- src/LinkList/mergeKLists.java
+++ src/LinkList/mergeKLists.java
@@ -27,8 +27,38 @@ public class mergeKLists {
         ListNode next;
         ListNode(int x) { val = x; }
     }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //暴力 先搞出来两个链表相加 然后从i=1开始 每一位的值都等于加上前一位的值 最后返回末尾呢个
+        if(l1==null && l2!=null)
+            return l2;
+        if(l1!=null && l2==null)
+            return l1;
+        if(l1 ==null && l2==null)
+            return null;
+        ListNode fake=new ListNode(0);
+        ListNode res=fake;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                res.next=l1;
+                l1=l1.next;
+            }else {
+                res.next=l2;
+                l2=l2.next;
+            }
+            res=res.next;
+        }
+        if(l1!=null)
+            res.next=l1;
+        else
+            res.next=l2;
+        return fake.next;
+    }
     public ListNode mergeKLists(ListNode[] lists) {
-
-        return null;
+        if(lists==null ||lists.length==0)
+            return null;
+        for(int i=1;i<lists.length;i++){
+            lists[i]=mergeTwoLists(lists[i],lists[i-1]);
+        }
+        return lists[lists.length-1];
     }
 }
