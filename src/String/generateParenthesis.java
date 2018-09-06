3c0eac71a5e3573f395bcb67a069963628ce8368
Imissrain <490646528@qq.com>
2018-09-06 14:09:57 +0800

    递归咯

--- src/String/generateParenthesis.java
+++ src/String/generateParenthesis.java
@@ -1,5 +1,8 @@
 package String;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,8 +22,41 @@ import java.util.List;
  */
 @SuppressWarnings("all")
 public class generateParenthesis {
+    //递归咯
     public List<String> generateParenthesis(int n) {
+        List<String> res=new ArrayList<>();
+        getAll(new char[n*2],0,res);
+        return res;
+    }
+    public void getAll(char current[],int pos,List<String> result){
+        if(pos==current.length){
+            if(isvaliad(new String(current))){
+                result.add(new String(current));
+            }
+        }else {
+            current[pos]='(';
+            getAll(current,pos+1,result);
+            current[pos]=')';
+            getAll(current,pos+1,result);
+        }
 
-        return null;
+    }
+    public boolean isvaliad(String str){
+        int balance=0;
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(c=='(')
+                balance++;
+            else
+                balance--;
+            if(balance<0)
+                return false;
+        }
+        return balance==0;
+    }
+
+    public static void main(String[] args) {
+        int n=3;
+        System.out.println(new generateParenthesis().generateParenthesis(n));
     }
 }
