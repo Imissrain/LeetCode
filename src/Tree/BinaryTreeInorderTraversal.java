5d1bdd8b4d88a3db4da34f33d211185a4d2fcc94
Imissrain <490646528@qq.com>
2018-10-25 12:18:50 +0800

    递归转化为迭代 用栈替代 当树不为空或者栈不为空时循环 首先走到 树的最左下端 每一步push当前节点 栈不为空保存然后跳转到右子树

--- src/Tree/BinaryTreeInorderTraversal.java
+++ src/Tree/BinaryTreeInorderTraversal.java
@@ -23,7 +23,7 @@ import java.util.Stack;
  * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
  */
 @SuppressWarnings("all")
-public class inorderTraversal {
+public class BinaryTreeInorderTraversal {
     public class TreeNode {
         int val;
         TreeNode left;
