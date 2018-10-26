faa0cc6892c7e65efbf6ad4cd9a2cdcbaa501038
Imissrain <490646528@qq.com>
2018-10-26 22:07:36 +0800

    二叉搜索树中序遍历结果就是他的顺序结果

--- src/Tree/KthSmallestElementinaBST.java
+++ src/Tree/KthSmallestElementinaBST.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * 230. 二叉搜索树中第K小的元素
  * 题目描述提示帮助提交记录社区讨论阅读解答
- * 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+ * 给定一个二叉搜索树，编写一个函数 KthSmallestElementinaBST 来查找其中第 k 个最小的元素。
  * <p>
  * 说明：
  * 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
@@ -31,12 +31,12 @@ import java.util.*;
  * 1
  * 输出: 3
  * 进阶：
- * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+ * 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 KthSmallestElementinaBST 函数？
  *
  * @author gkyan
  */
 @SuppressWarnings("all")
-public class kthSmallest {
+public class KthSmallestElementinaBST {
     public class TreeNode {
         int val;
         TreeNode left;
