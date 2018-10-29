ca2003c096611e8776fd46804f31f3f8df4823ae
Imissrain <490646528@qq.com>
2018-10-29 13:44:55 +0800

    小于k直接反转 然后每次截取2k个长度进行操作 操作完成后 字符串每次截断2k长度 当长度小于2k时 反转前k即可

--- src/String/ReverseStringII.java
+++ src/String/ReverseStringII.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 
 /**
  *
- New Playground
- Imissrain
-
  541. 反转字符串 II
  题目描述提示帮助提交记录社区讨论阅读解答
  给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。如果剩余少于 k 个字符，则将剩余的所有全部反转。
@@ -22,7 +19,7 @@ import java.util.ArrayList;
  给定字符串的长度和 k 在[1, 10000]范围内。
  * */
 @SuppressWarnings("all")
-public class reverseStrII {
+public class ReverseStringII {
     public String reverseStr(String s, int k) {
         //小于k直接反转 然后每次截取2k个长度进行操作 操作完成后 字符串每次截断2k长度 当长度小于2k时 反转前k即可
         if(s.length()<k)//小于k直接反转
