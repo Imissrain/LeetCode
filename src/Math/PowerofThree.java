1f958c8182214b66e71e11a2e6366c5a96e24413
Imissrain <490646528@qq.com>
2018-11-03 14:13:42 +0800

    余3除3

--- src/Math/PowerofThree.java
+++ src/Math/PowerofThree.java
@@ -1,6 +1,27 @@
 package Math;
+/**
+ * 26. 3的幂
+ 题目描述提示帮助提交记录社区讨论阅读解答
+ 给定一个整数，写一个函数来判断它是否是 3 的幂次方。
+
+ 示例 1:
+
+ 输入: 27
+ 输出: true
+ 示例 2:
+
+ 输入: 0
+ 输出: false
+ 示例 3:
+
+ 输入: 9
+ 输出: true
+ 示例 4:
+
+ 输入: 45
+ 输出: false*/
 @SuppressWarnings("all")
-public class isPowerOfThree {
+public class PowerofThree {
 
     public boolean isPowerOfThree(int n) {
         //抖机灵
