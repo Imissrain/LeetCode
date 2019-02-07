9ba3fbe7c9633dd6288b78f9bdd4494ce6cb7362
Imissrain <geeker_yan@163.com>
2019-02-07 13:16:08 +0800

    indexOf || 哈希表

--- src/String/FirstUniqueCharacterinaString.java
+++ src/String/FirstUniqueCharacterinaString.java
@@ -17,6 +17,18 @@ import java.util.Map;
  * */
 @SuppressWarnings("all")
 public class FirstUniqueCharacterinaString {
+    /*
+    public int indexOf(int ch): 返回指定字符在字符串中第一次出现处的索引，如果此字符串中没有这样的字符，则返回 -1。
+
+    public int indexOf(int ch, int fromIndex): 返回从 fromIndex 位置开始查找指定字符在字符串中第一次出现处的索引，如果此字符串中没有这样的字符，则返回 -1。
+
+    int indexOf(String str): 返回指定字符在字符串中第一次出现处的索引，如果此字符串中没有这样的字符，则返回 -1。
+
+    int indexOf(String str, int fromIndex): 返回从 fromIndex 位置开始查找指定字符在字符串中第一次出现处的索引，如果此字符串中没有这样的字符，则返回 -1。
+     */
+    /*
+     version1:indexOf
+     */
     public static int firstUniqChar(String s) {
        for(int i=0;i<s.length();i++){
            char tmp=s.charAt(i);
@@ -24,6 +36,21 @@ public class FirstUniqueCharacterinaString {
                return i;
        }
        return -1;
+    }
+
+    /*
+      version2:哈希表
+     */
+    public static int firstUniqChar1(String s) {
+        int[] arr=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<s.length();i++){
+            if(arr[s.charAt(i)-'a']==1)
+                return i;
+        }
+        return -1;
     }
     public static void main(String[] args){
         String s="lovelive";
