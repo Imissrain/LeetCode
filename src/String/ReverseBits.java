9f74ac67515e98e5326d7e2ea51686fbbca35b5d
Imissrain <geeker_yan@163.com>
2019-03-11 17:42:50 +0800

    取x绝对值进行反转  记得用long型来存 避免溢出 返回时存成int类型 反转核心 res=res*10+abs%10 abs/=10  反转负数多乘-1即可

--- src/String/ReverseBits.java
+++ src/String/ReverseBits.java
@@ -20,6 +20,9 @@ package String;
  * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。根据这个假设，如果反转后的整数溢出，则返回 0。
  * */
 @SuppressWarnings("all")
+/*
+   取x绝对值进行反转  记得用long型来存 避免溢出 返回时存成int类型 反转核心 res=res*10+abs%10 abs/=10  反转负数多乘-1即可
+ */
 public class ReverseBits {
     public int reverse(int x) {
         if(x==0)
