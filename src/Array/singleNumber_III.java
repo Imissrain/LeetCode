040cadf66616b3ae7d0d134cf4847cc73428a882
Imissrain <geeker_yan@163.com>
2019-04-05 11:48:48 +0800

    res=0 首先先逐个异或一遍 这个时候res是那两个数字的异或结果 既然两个数字不同     那么肯定至少有一位为1 res&=-res 这个操作可以找到最低的那个有效位1 根据这个将原数组分为两部分     再分别异或 就可以找到两个数字

--- src/Array/singleNumber_III.java
+++ src/Array/singleNumber_III.java
@@ -18,6 +18,11 @@ import org.junit.Test;
  * @author gkyan
  */
 @SuppressWarnings("all")
+/*
+    res=0 首先先逐个异或一遍 这个时候res是那两个数字的异或结果 既然两个数字不同
+    那么肯定至少有一位为1 res&=-res 这个操作可以找到最低的那个有效位1 根据这个将原数组分为两部分
+    再分别异或 就可以找到两个数字
+ */
 public class singleNumber_III {
     //先res整个异或一边 得到两个数字的异或值 然后res&-res 找到最低有效位 然后根据最低有效位来将数组分成两部分 分别异或 得到答案
     public int[] singleNumber(int[] nums) {
@@ -38,10 +43,8 @@ public class singleNumber_III {
     }
     @Test
     public void fun(){
-
-         System.out.println(4^5);
-         System.out.println(Integer.toBinaryString(1));
-         System.out.println(Integer.toBinaryString(-1));
-
+        int res=3^5;
+        System.out.println(Integer.toBinaryString(6));
+        System.out.println(Integer.toBinaryString(-6));
     }
 }
